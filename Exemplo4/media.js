cdb276870386558cd6e27ea2dd4b2a0ce4c045fd
Prof. Hugo <hugo.tannus@go.senac.br>
2024-04-25 17:40:48 -0300

    Encapsulamento de funções.

--- Exemplo4/media.js
+++ Exemplo4/media.js
@@ -1,9 +1,14 @@
-export function verificaSituacao(n1, n2, n3, n4, p1=1, p2=1, p3=1, p4=1) {
-    let media = (n1*p1 + n2*p2 + n3*p3 + n4*p4) / (p1 + p2 + p3 + p4);
+export function calculaMedia(n1, n2, n3, n4, p1=1, p2=1, p3=1, p4=1){
+    return (n1*p1 + n2*p2 + n3*p3 + n4*p4) / (p1*1 + p2*1 + p3*1 + p4*1);
+}
 
-    if(media >= 7) {
-        return "Aprovado"
-    } else {
-        return "Reprovado"
-    }
+export function verificaAprovacao(media, presenca = 0.75) {
+    return media >= 7 && presenca >= 0.75 ? "Aprovado" : "Reprovado"
+}
+
+export function verificaSituacao(n1, n2, n3, n4, p1=1, p2=1, p3=1, p4=1) {
+    let media = calculaMedia(n1, n2, n3, n4, p1, p2, p3, p4);
+    let situacao = verificaAprovacao(media);
+
+    return situacao
 }
