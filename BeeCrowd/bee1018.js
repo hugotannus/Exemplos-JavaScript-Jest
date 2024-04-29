6ae0f20478b85d51f6db94a45c1163f60ea41cbd
Prof. Hugo <hugo.tannus@go.senac.br>
2024-04-29 17:46:54 -0300

    Resolução do problema BeeCrownd 1018

--- BeeCrowd/bee1018.js
+++ BeeCrowd/bee1018.js
@@ -1,4 +1,19 @@
-export function solucao(lines) {
-    // resolução do problema
-    return [];
+const cedulas = [100, 50, 20, 10, 5, 2, 1]
+
+export function solucao(v) {
+    const result = [];
+    let valor = v;
+
+    for(var i=0; i < cedulas.length; i++) {         // i = 6;
+        let cedula = cedulas[i];                    // cedula = 1;
+        let resto = valor % cedula;                 // resto = 0;
+        let dividendo = valor - resto               // dividendo = 0;
+        let qtdNotas = dividendo / cedula;          // qtdNotas = 0;
+
+        result[i] = qtdNotas;               // resut [112,1,0,0,1,1,0]
+
+        valor = resto;                      // valor = 0
+    }
+
+    return result;
 }
--- BeeCrowd/bee1018.test.js
+++ BeeCrowd/bee1018.test.js
@@ -1,19 +1,38 @@
+
+import { solucao } from "./bee1018"
+
 describe('Cenários de Teste', () => {
     const cenarios = [
         {
-            lines: ["567"],
+            valor: 0,
+            result: [0,0,0,0,0,0,0]
+        },
+        {
+            valor: 100,
+            result: [1,0,0,0,0,0,0]
+        },
+        {
+            valor: 50,
+            result: [0,1,0,0,0,0,0]
+        },
+        {
+            valor: 188,
+            result: [1,1,1,1,1,1,1]
+        },
+        {
+            valor: 567,
             result: [5,1,0,1,1,1,0]
         },
         {
-            lines: ["576"],
+            valor: 576,
             result: [5,1,1,0,1,0,1]
         },
         {
-            lines: ["11257"],
+            valor: 11257,
             result: [112,1,0,0,1,1,0]
         },
         {
-            lines: ["503"],
+            valor: 503,
             result: [5,0,0,0,0,1,1]
         },
     ]
@@ -22,43 +41,8 @@ describe('Cenários de Teste', () => {
         let tituloDoTeste = 'Caso #' + i;
         
         test(tituloDoTeste, function() {
-            // let { lines, result } = cenario;
-
-            // let output = resolve(lines);
-
-            // expected(output).toBe(result);
-            expect(resolve(cenario.lines)).toBe(cenario.result)
+            expect(solucao(cenario.valor)).toEqual(cenario.result)
         });
     })
 });
 
-import { solucao } from "./bee1018"
-
-describe('BeeCrowd 1018 - "Cédulas', () => {
-    describe('Casos de Teste', () => {
-        test('Caso #1', () => {
-            let entrada = ["567"];
-            let saidaEsperada = `576
-                5 nota(s) de R$ 100,00
-                1 nota(s) de R$ 50,00
-                1 nota(s) de R$ 20,00
-                0 nota(s) de R$ 10,00
-                1 nota(s) de R$ 5,00
-                0 nota(s) de R$ 2,00
-                1 nota(s) de R$ 1,00`
-
-            let saida = solucao(entrada);
-
-            expect(saida).toBe(saidaEsperada);
-        });
-
-        test('Caso #2', () => {
-            let entrada = ["217 14 6"];
-            let saidaEsperada = "217 eh o maior"
-
-            let saida = solucao(entrada);
-
-            expect(saida).toBe(saidaEsperada);
-        });
-    });
-});
